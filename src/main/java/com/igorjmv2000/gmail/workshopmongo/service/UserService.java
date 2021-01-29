43c0d87c2d2b2e0e0275e5e5246c75d3dcf02c95
Igor Marinho <igorjmv2000@gmail.com>
2021-01-29 16:05:32 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/igorjmv2000/gmail/workshopmongo/service/UserService.java
+++ src/main/java/com/igorjmv2000/gmail/workshopmongo/service/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repository.insert(entity);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO dto) {
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
