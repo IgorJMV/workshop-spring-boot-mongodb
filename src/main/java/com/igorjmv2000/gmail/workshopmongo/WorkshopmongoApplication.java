1ffa80be386adfaf361b19ddf0c09d63dfade2a8
Igor Marinho <igorjmv2000@gmail.com>
2021-01-29 14:39:42 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/igorjmv2000/gmail/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.igorjmv2000.gmail.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorjmv2000/gmail/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorjmv2000.gmail.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
