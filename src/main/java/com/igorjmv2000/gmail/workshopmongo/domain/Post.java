037ddf100d805e1b10e4dc52f258ab2bc8fd2334
Igor Marinho <igorjmv2000@gmail.com>
2021-02-19 17:32:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/igorjmv2000/gmail/workshopmongo/domain/Post.java
+++ src/main/java/com/igorjmv2000/gmail/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.igorjmv2000.gmail.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.igorjmv2000.gmail.workshopmongo.dto.AuthorDTO;
+import com.igorjmv2000.gmail.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String tittle;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -67,6 +72,10 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
